d26dc0aec32e851c6d52aeaf1bef64916510a98a
barbaragrcast <barbaragrcast@gmail.com>
2025-10-01 23:26:56 +0000

    Added content

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-print("Welcome to our amusement Park!")
+print("Welcome to American Bank")
+print("As a new user, we are here to help setting up your first account")
 name = input("Please enter your name: ")
-age = input(f"Hello {name}, please enter your age: ")
+age = int(input(f"Hello {name}, please enter your age: "))
+
+if age <= 18:
+    print("Sorry, you are to young to acess this service")
+
 
 while True:
-    print("\nEnter your choice")
-    print("1. Option One (placeholder)")
-    print("2. Option Two (placeholder)")
-    print("3. Option Three (placeholder)")
+    print("Choose an option")
+    print("1. Types of accounts")
+    print("2. Create account")
+    print("3. View account")
     print("4. Exit")
 
     choice = input("Choose an option (1-4): ")
 
     if choice == "1":
-        print("You picked Option 1")
+        print("There are 4 main types of bank accounts. ")
+        print("1. Savings account ")
+        print("2. Checking account ")
+        print("3. Money Market account ")
+        print("1. Certificates of Deposit account ")
+        accountChoice = input("please chose one you will like to know more about:")
     elif choice == "2":
         print("You picked Option 2")
     elif choice == "3":
